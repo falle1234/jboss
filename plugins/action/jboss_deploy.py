3bcbb4e805d35a4b029ac07b7840cdac3a7eeb95
Søren Fallesen <falle1234@gmail.com>
2022-10-10 20:57:59 +0200

    Fixed missing tmp_dir from action

--- plugins/action/jboss_deploy.py
+++ plugins/action/jboss_deploy.py
@@ -14,8 +14,10 @@ class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
         tmp_remote_src = self._make_tmp_path()
         result = super(ActionModule, self).run(tmp, task_vars)
+        module_args = self._task.args.copy()
+        module_args.update(dict(tmp_dir=tmp_remote_src,))
         self._transfer_file(self._task.args['deployment_file'], tmp_remote_src + self._task.args['deployment']+'.ear')
-        result.update(self._execute_module(module_name='jboss_deploy',module_args=self._task.args.copy(),task_vars=task_vars))
+        result.update(self._execute_module(module_name='jboss_deploy',module_args=module_args,task_vars=task_vars))
         result['deployment_file'] = self._task.args['deployment_file']
         result['temp_file'] = tmp_remote_src + self._task.args['deployment']+'.ear'
         return result
